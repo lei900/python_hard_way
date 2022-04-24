fd846d96c13ee95709ac7968c4e52b94fd49e34c
lei <mia.wang90@gmail.com>
2022-04-24 22:41:25 +0900

    ex9-10

---
+++ ex9.py
@@ -0,0 +1,33 @@
+# ex9
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print("Here are the days: ", days)
+print("Here are the months ", months)
+
+print("""
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""")
+
+# ex10
+# type "\" with option + ¥
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a lie."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
+
+# ex11
